9844933c08244debf1f428ccc3ada89e31c8d4fe
ALbert Emidio <albert.cosme22@gmail.com>
2026-02-14 19:27:31 -0300

    aula_2

---
+++ Aula_02/VariavelPrimitiva.java
@@ -0,0 +1,41 @@
+public class VariavelPrimitiva {
+    public static void main(String[] args) {
+
+        // int → número inteiro
+        int numero = 25;
+
+        // float → número decimal (precisa do 'f' no final)
+        float numeroFloat = 25.5f;
+
+        // double → número decimal com maior precisão
+        double numeroDouble = 10.75;
+
+        // char → armazena um único caractere (entre aspas simples)
+        char letra = 'A';
+
+        // boolean → verdadeiro ou falso
+        boolean ativo = true;
+
+        // byte → inteiro pequeno (-128 até 127)
+        byte numeroByte = 100;
+
+        // short → inteiro médio
+        short numeroShort = 32000;
+
+        // long → inteiro grande (precisa do 'L' no final)
+        long numeroLong = 1000000000L;
+
+        // final → constante (não pode ser alterada)
+        final double PI = 3.14;
+
+        System.out.println( numero);
+        System.out.println(numeroFloat);
+        System.out.println(numeroDouble);
+        System.out.println(letra);
+        System.out.println(ativo);
+        System.out.println(numeroByte);
+        System.out.println(numeroShort);
+        System.out.println(numeroLong);
+        System.out.println(PI);
+    }
+}
